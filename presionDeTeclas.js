ccbab30349cd4c1d7ed1d3e01ab9f07a79ffb4a7
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-01-16 21:39:57 +0100

    controlar el guerrero con las teclas "espacio" y "->"

--- presionDeTeclas.js
+++ presionDeTeclas.js
@@ -1,18 +1,50 @@
-document.addEventListener("keypress", pulsarTecla);
+
+document.addEventListener("keydown", pulsarTecla);
+document.addEventListener("keyup", soltarTecla);
 
 
+// ATACA AL SPACIO
 
 function pulsarTecla(event) {
-    console.log(event)
+    console.log(event);
+
     if (event.keyCode === 32) {
 
-        event.repeat = true;
         let divGuerrero = document.querySelector(".MedievalWarriorParado");
         divGuerrero.removeAttribute("class");
         divGuerrero.classList.add("MedievalWarriorAtacando");
+
+    }
+
+    if (event.keyCode === 39) {
+
+        let divGuerrero = document.querySelector(".MedievalWarriorParado");
+        divGuerrero.removeAttribute("class");
+        divGuerrero.classList.add("MedievalWarriorCorriendo");
+
+    }
+
+
+
+}
+//SI SE SUELTA LA TECLA VUELVE AL ESTADO ORIGINAL [PARADO]
+function soltarTecla(event) {
+    console.log("tecla soltada")
+
+    if (event.keyCode === 32) {
+        let divGuerrero = document.querySelector(".MedievalWarriorAtacando");
+        divGuerrero.removeAttribute("class");
+        divGuerrero.classList.add("MedievalWarriorParado");
+    }
+
+    if (event.keyCode === 39) {
+        let divGuerrero = document.querySelector(".MedievalWarriorCorriendo");
+        divGuerrero.removeAttribute("class");
+        divGuerrero.classList.add("MedievalWarriorParado");
 
     }
 }
 
 
 window.onload = pulsarTecla;
+window.onload = soltarTecla;
