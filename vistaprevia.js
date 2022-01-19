af3118d99313040045bcadb0b051c7669dcdbf47
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-01-19 10:37:50 +0100

    Tarjetero del medio y es el unico que se tiene que mover

---
+++ vistaprevia.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+function pulsado1() {
+
+    let cartaVP = document.querySelector("#vistaPrevia");
+    cartaVP.classList.remove();
+    cartaVP.classList.add("MedievalWarriorParado");
+
+}
+
+
