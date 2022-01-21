3ba912b73ed008d4f7182b6af9588fe9d9da78b9
Álvaro Mena Parra <alvaromepa@gmail.com>
2022-01-21 18:38:59 +0100

    Añadido de la vida y mana

--- vistaprevia.js
+++ vistaprevia.js
@@ -5,10 +5,10 @@ function pulsado1() {
 
     resetPage();
 
-    let cartaVP = document.querySelector("#vistaPrevia");
-
+    let cartaVP = document.querySelector("#vistaPrevia");//se obtiene vista previa
 
     cartaVP.classList.remove();
+
     cartaVP.classList.add("MedievalWarriorParado");
 
     let boton = document.createElement("button");
@@ -29,21 +29,17 @@ function gritar() {
 }
 
 
+
 function barraDeVida() {
-    let divVida = document.querySelector("#vida");
-    let vida = document.createElement("progress");
-    vida.setAttribute("value", "60");
-    vida.setAttribute("max", "100");
-    divVida.appendChild(vida);
+
+    let divVida = document.getElementById("vida");
+    divVida.classList.add("vida");
+
 
 }
 function mana() {
-    let divMana = document.querySelector("#mana");
-    let mana = document.createElement("progress");
-    mana.setAttribute("value", "30");
-    mana.setAttribute("max", "100");
-    divMana.appendChild(mana);
-
+    let divVida = document.getElementById("mana");
+    divVida.classList.add("mana");
 }
 
 function resetPage() {
